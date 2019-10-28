4c1fa2706c31f8e9998120c10922c732bf74edf9
SURFACE <SURFACE@10.70.2.239>
2019-10-28 11:44:55 -0600

    10/28/19

---
+++ CS1050/src/LinkedList.java
@@ -0,0 +1,255 @@
+/**
+ *A linked list class that implements MyCollectinoInterface for project 4 
+ *Calvin Nguyen and Osman Rakhimov
+ *list class
+ *Eclipse Java IDE
+ *insouciant:showing a casual lack of concern; indifferent.
+ *"Life begin at the end of your Comfort Zone. So if you`re feeling uncomfortable right now, 
+ *	know that the change taking place in your life is a begining, not an ending" - Neale Walsch, (9.1943)
+ *@author Calvin Nguyen and Osman Rakhimov
+ *@version project 4 
+ *@class LinkedList
+ */
+
+public class LinkedList<T> {
+	private Node<T> head;
+	private Node<T> tail;
+	private int listCount = 0;
+	private Node<T> currentNode = head;
+	private Node<T> nodeBefore;
+	private Node<T> nodeAfter;
+	private int numberOfEntries;
+	private boolean result = false;
+	
+//************************************************************************* 	
+	/**
+	  * Adds a new entry to this collection
+	  * 
+	  * @param newItem The object to be added to the collection
+	  * @return True if the addition is successful, or false if not.
+	  */
+	
+	public boolean add(T newItem) {
+		
+		Node newNode = new Node(newItem);
+		
+		newNode.next = head;
+		head = newNode;
+		
+		if (head.equals(newNode)) {
+			numberOfEntries ++;
+		}
+		return result;
+		
+	}
+	
+//*************************************************************************  
+    /**
+     * Adds a new entry to this collection at the given position
+     * 
+     * @param newItem The object to be added to the collection
+     * @param position The location where newItem will be inserted.
+     * @return True if the addition is successful, or false if not.
+     */
+	
+	public boolean add(T newItem, int givenPosition) {
+		result = false;
+		
+		if((givenPosition >= 0) && (givenPosition <= numberOfEntries)) {
+			
+			Node<T> newNode = new Node<T> (newItem);
+			
+			if (givenPosition == 0) {
+				newNode.next = head;
+				head = newNode;
+				numberOfEntries ++;
+				add(newItem);
+				result = true;
+			}
+
+			else {
+				Node<T> currentNode = head;
+				
+				for (int i = 1;i < givenPosition; i++) {
+					currentNode = currentNode.next;
+				}
+				
+				newNode.next = currentNode.next;
+				currentNode.next = newNode;
+				numberOfEntries++;
+			}// end of if
+		}
+		else {
+				throw new IndexOutOfBoundsException("Wrong position given");
+			}
+			return result;
+	}
+	
+ //*************************************************************************
+    
+ 	/**
+ 	 * Removes one occurrence of a given entry from this collection.
+ 	 *
+ 	 * @param anEntry The entry to be removed.
+ 	 * @return True if the removal was successful, or false if not.
+ 	 */
+	
+
+	public boolean remove(T anEntry) {
+		result = false;
+        currentNode = head;
+        nodeBefore = null;
+        nodeAfter = null;
+        try{
+        if(isEmpty()){
+           throw new IllegalArgumentException("List is empty");
+        }
+        nodeAfter = head.getNextNode();
+        if(currentNode.getData().equals(anEntry)){
+           head = currentNode.getNextNode();
+           result = true;
+           listCount--;
+        }
+        else{
+           try{
+              while(currentNode != null && !currentNode.getData().equals(anEntry)){
+                 nodeBefore = currentNode;
+                 currentNode = currentNode.getNextNode();
+                 nodeAfter = currentNode.getNextNode();
+                 
+              }
+              if(currentNode != null){
+                 listCount--;
+                 result = true;
+                 nodeBefore.setNextNode(nodeAfter);
+                 currentNode.setNextNode(null);
+              }
+            }
+            catch(NullPointerException e){
+              System.err.println("Node is null");
+            }
+         }
+        }
+        catch(IllegalArgumentException e){
+        System.err.println("No item found in list");
+        }    
+        return result;
+	}
+
+	
+//*************************************************************************
+	
+    /**
+     * Removes all entries from this collection.
+     */
+	
+	public void clear() {
+		head = null;
+		numberOfEntries = 0;
+	}
+	
+//*************************************************************************
+	
+	 /**
+     * Gets the current number of entries in this collection.
+     *
+     * @return The integer number of entries currently in the collection.
+     */
+
+	public int getCurrentSize() {
+		Node <T> current = head;
+    	while (current.next != null) {
+    		listCount +=1;
+    		current = current.next;		
+    	}
+    	return listCount;
+    }
+	
+//*************************************************************************  
+    
+    /**
+     * Check to see if the collection is empty.
+     *
+     * @return True if the collection is empty, or false if not.
+     */
+	
+	public boolean isEmpty() {
+		result = false;
+		
+		if(numberOfEntries==0) {
+			result = true;
+		}
+		return result;
+	}// end of empty
+	
+//*************************************************************************     
+    /**
+     * Counts the number of times a given entry appears in this collection.
+     *
+     * @param anEntry The entry to be counted.
+     * @return The number of times anEntry appears in the collection.
+     */
+	
+	public int getFrequencyOf(T anEntry) {
+		Node<T> currentNode = new Node<T> (anEntry);
+		for(int i = 0; i<= numberOfEntries; i++) {
+			if(anEntry.equals(currentNode.getData())) {
+				listCount++;
+			}
+			currentNode = currentNode.getNextNode();
+			
+		}
+		return listCount;
+	}
+
+//************************************************************************* 
+    
+    /**
+     * Tests whether this collection contains a given entry.
+     *
+     * @param anEntry The entry to locate.
+     * @return True if the collection contains anEntry, or false if not.
+     */
+	
+	public boolean contains(T anEntry) {
+		
+		result = false;
+		
+		Node<T> currentNode = head;
+		
+		while(!result && (currentNode != null)) {
+			if(anEntry.equals(currentNode.getData())) {
+				result = true;
+			}
+			else {
+				currentNode = currentNode.getNextNode();
+			}// end of if
+			
+						
+		}// end of while
+		return result;
+	}// end of contains
+
+//************************************************************************* 
+	   
+    /**
+     * Retrieves all entries that are in this collection.
+     *
+     * @return A newly allocated array of all the entries in the collection. 
+     * Note: If the collection is empty, the returned array is empty.
+     */
+	
+	public Object[] toArray() {
+		Object[] thing =  new Object[numberOfEntries];
+	      int index = 0;
+	      Node<T> currentNode = head;
+	      while((index < numberOfEntries) && (currentNode != null)){
+	         thing[index] = currentNode.getData();
+	         currentNode = currentNode.getNextNode();
+	         index++;
+	      }
+	      return thing;   
+	    
+	    } // End of method
+	
+}
